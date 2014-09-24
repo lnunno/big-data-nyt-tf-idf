7260c06bc8c47eee6e111f65ae481182fd0691f6
lnunno <lucasnunno@gmail.com>
2014-09-24 12:46:12 -0600

    Data with 40000 article abstracts.

---
+++ src/nytnlp/export.py
@@ -0,0 +1,19 @@
+'''
+Combines and exports the JSON files produced by get_data.py to a format friendly
+to Hadoop.
+
+Created on Sep 24, 2014
+
+@author: lnunno
+'''
+
+def combine(files, output_file):
+    '''
+    @param files: A list of JSON file paths
+    
+    @param output_file: A file path of the destination file.  
+    '''
+    pass
+
+if __name__ == '__main__':
+    pass
--- src/nytnlp/get_data.py
+++ src/nytnlp/get_data.py
@@ -1,4 +1,9 @@
 '''
+Downloads articles as JSON and combines the results into a single JSON file to
+be processed later.
+
+@see: export.py 
+
 Created on Sep 23, 2014
 
 @author: lnunno
@@ -7,7 +12,7 @@ import os
 import requests
 import json
 import time
-from nytnlp.secret import API_KEY
+from nytnlp.secret import API_KEY, API_KEY2
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.abspath(os.path.join(THIS_DIR,'..','..','data'))
@@ -16,9 +21,10 @@ VERSION = 'v3'
 BASE_URL = 'http://api.nytimes.com/svc/news/%s/content/all/all.json' % (VERSION)
 
 def main(DEBUG=False):
+    initial_offset = 40000
     requests_per_second = 8
     sleep_time = 1/requests_per_second
-    offset = 0
+    offset = 0 + initial_offset
     num_results = 100 if DEBUG else 50000
     print('Going to collect %5d articles.' %(num_results))
     file_name = 'articles_sample.json' if DEBUG else 'articles.json'
@@ -26,15 +32,15 @@ def main(DEBUG=False):
     assert os.path.isdir(DATA_DIR)
     params = {
               'api-key': API_KEY,
-              'limit':num_results,
-              'offset':offset
+              'offset' :  offset
               }
     results = []
     while num_results > 0:
+        print('Current offset = %d' %(offset))
         params['offset'] = offset
         r = requests.get(BASE_URL, params=params)
         if not r.status_code == requests.codes.ok: # @UndefinedVariable
-            print('HTTP Error code returned, trying again in %f seconds...' %(sleep_time))
+            print('HTTP Error code %d returned, trying again in %f seconds...' %(r.status_code, sleep_time))
             # Sleep a little bit and then try again.
             time.sleep(sleep_time)
             continue
@@ -42,7 +48,14 @@ def main(DEBUG=False):
         results += json_response['results']
         offset += 20
         num_results -= 20
-        print('Collected %5d results' %(offset))
+        num_collected = offset - initial_offset 
+        print('Collected %5d results' % (num_collected))
+        if (num_collected % 10000) == 0:
+            td = {'docs': results}
+            tfp = os.path.join(DATA_DIR,'articles_older_%d.json' % (num_collected))
+            with open(tfp,'wb') as f:
+                json.dump(td,f)
+                print('Saved temp results to %s' %(tfp))
         time.sleep(sleep_time) # Wait a little bit so we don't get banned.
     d = {'docs': results }
     with open(file_path, 'wb') as f:
