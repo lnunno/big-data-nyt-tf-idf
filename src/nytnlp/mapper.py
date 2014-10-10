a3d533c7a2c9ab7811ed16fd4fb57727ba700e8c
lnunno <lucasnunno@gmail.com>
2014-10-09 23:30:54 -0600

    Helpers for testing the mapper

--- src/nytnlp/mapper.py
+++ src/nytnlp/mapper.py
@@ -8,12 +8,44 @@ Created on Oct 9, 2014
 '''
 from nytnlp.preprocess import clean_text
 import sys
+from _io import StringIO
+import csv
+from argparse import ArgumentParser
 
 separator = '\t'
 
+
+def helper(line):
+    docid, abstract, url_str = parse_csv_string(line)
+    cleaned = clean_text(abstract)
+    for word in cleaned.split():
+        print(word, url_str)
+
 def main():
     for line in sys.stdin:
-        print(clean_text(line),separator,1)
+        helper(line)
+
+def parse_csv_string(s):
+    f = StringIO(s)
+    reader = csv.reader(f,delimiter=',')
+    ls = []
+    for row in reader:
+        ls.append(row)
+    return ls[0]
+        
+def test():
+    s1 = '0,"The premium-priced phone makes a strong case to make the switch, but its size and difficult to use keyboard are among its drawbacks.",http://www.nytimes.com/2014/09/25/technology/personaltech/blackberry-passport-review.html'
+    s2 = '1,The three men turned themselves in to the French authorities after a mix-up allowed them to walk free.,http://www.nytimes.com/2014/09/25/world/europe/french-islamic-militant-suspects-expelled-by-turkey.html'
+    print('---s1---')
+    helper(s1)
+    print('---s2---')
+    helper(s2)
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--test', dest='test', action='store_true', default=False)
+    args = parser.parse_args()
+    if args.test:
+        test()
+    else:
+        main()
