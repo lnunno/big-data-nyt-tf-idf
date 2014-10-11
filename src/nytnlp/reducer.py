852b50c0950b011023eec133ed78e37b44f64701
lnunno <lucasnunno@gmail.com>
2014-10-11 10:55:09 -0600

    Generating solid JSON output from the reducer now.

--- src/nytnlp/reducer.py
+++ src/nytnlp/reducer.py
@@ -8,6 +8,9 @@ import sys
 from collections import defaultdict
 import numpy as np
 import pandas as pd
+from _io import StringIO
+
+DEBUG = False
 
 class NYTReducer(object):
     
@@ -43,24 +46,32 @@ def main():
     freq_matrix = pd.DataFrame.from_dict(article_vectors)
     freq_matrix = freq_matrix.fillna(0) # Fill NaN values with a 0.
     
-    print('===Freq matrix===\n',freq_matrix)
+    if DEBUG: print('===Freq matrix===\n',freq_matrix)
     
     # Calculate the number of documents that each term appears in.
     df_vector = freq_matrix.apply(lambda a: np.nonzero(a)[0].size, axis=1)
-    print('===Doc freq vector===\n',df_vector)
+    if DEBUG: print('===Doc freq vector===\n',df_vector)
     
     idf_vector = np.log(reducer.number_of_articles/df_vector)
-    print('===IDF vector===\n',idf_vector)
+    if DEBUG: print('===IDF vector===\n',idf_vector)
     
     # Normalizes the DataFrame with the "augmented" term-frequency.
-    norm_freq_matrix = freq_matrix + 0.5 + ((0.5 * freq_matrix)/freq_matrix.max())
-    print(norm_freq_matrix)
+    norm_freq_matrix = 0.5 + ((0.5 * freq_matrix)/freq_matrix.max())
+    if DEBUG: print('===Norm Freq matrix===\n',norm_freq_matrix)
     
     tf_idf_matrix = norm_freq_matrix.mul(idf_vector, axis=0)
+    if DEBUG: print('===TF-IDF Matrix===\n',tf_idf_matrix)
     
-    print('===TF-IDF Matrix===\n',tf_idf_matrix)
+    def output_func(col):
+        '''
+        This function outputs the result of the reducer. Namely, the tf-idf vector for every abstract.
+        '''
+        s = StringIO()
+        col.to_json(s)
+        print('%s\t%s' % (col.name,s.getvalue()))
     
-
+    
+    tf_idf_matrix.apply(output_func, axis=0)
     
     
     
