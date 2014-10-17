96dd4b497bd9859a30a2ec911e5ac222a8f190ac
lnunno <lucasnunno@gmail.com>
2014-10-16 22:20:56 -0600

    Spark output options

--- src/nytnlp/test_mapper.sh
+++ src/nytnlp/test_mapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATA="../../data/articles_sample.csv"
 SM_DATA=$(head -n 5 $DATA)
-cat $DATA | ./mapper.py 
+cat $DATA | ./mapper.py
--- src/nytnlp/tf_idf_mapper.py
+++ src/nytnlp/tf_idf_mapper.py
@@ -11,8 +11,15 @@ import pandas as pd
 import numpy as np
 from pandas.io.json import read_json
 from _io import StringIO
+from argparse import ArgumentParser
+
+NONZERO_ONLY = False
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('--spark', dest='spark', action='store_true', default=False)
+    args = parser.parse_args()
+    
     doc_freq = read_json('../../data/doc_freq.json', typ='series')
     N = doc_freq['TOTAL_DOCS']
     idf_vector = np.log10(N / doc_freq)
@@ -23,17 +30,25 @@ def main():
         # Calculate tf-idf from tf vector and doc_freq vector 
         tf_idf_vector = tf_vector.multiply(idf_vector, fill_value=0)
         
-        # tf_idf terms where these terms actually exist in this document, since
-        # this is going to be a sparse vector.
-        tf_idf_nonzero_vector = tf_idf_vector[tf_idf_vector.nonzero()[0]]
-        
+        if NONZERO_ONLY:
+            # tf_idf terms where these terms actually exist in this document, since
+            # this is going to be a sparse vector.
+            output_vector = tf_idf_vector[tf_idf_vector.nonzero()[0]]
+        else:
+            output_vector = tf_idf_vector
+            
         # Normalize the tf-idf vector, this is important for the clustering done
         # later on.
-        tf_idf_norm_vector = tf_idf_nonzero_vector / tf_idf_nonzero_vector.sum() 
+        output_vector = output_vector / output_vector.sum() 
         
         s = StringIO()
-        tf_idf_norm_vector.to_json(s)
-        print('%s\t%s' % (url, s.getvalue()))  # The tf-idf vector.
+        
+        if args.spark:
+            output_vector.to_json(s)
+            print('%s\t%s' % (url, s.getvalue()))
+        else:
+            output_vector.to_csv(s,index=False)
+            print('%s' % (s.getvalue()))
         
 if __name__ == '__main__':
     main()
